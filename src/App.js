44da3f669b588cbadce2200d2f72dfbf2e2c3c17
anikshajol <anikshajol@gmail.com>
2022-10-11 01:31:34 +0600

    navbar with responsive added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import Home from "./Components/Home/Home";
+import Statistics from "./Components/Statistics/Statistics";
+import Blog from "./Components/Blog/Blog";
+import Main from "./Layouts/Main";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+          path: "/",
+          element: <Home></Home>,
+        },
+        {
+          path: "home",
+          element: <Home></Home>,
+        },
+        {
+          path: "/statistics",
+          element: <Statistics></Statistics>,
+        },
+        {
+          path: "/blog",
+          element: <Blog></Blog>,
+        },
+      ],
+    },
+  ]);
+
   return (
     <div className="App">
-      <h2>Hello world!</h2>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <div>
-      <h2>Header</h2>
+    <div className="bg-blue-100 py-4 font-bold">
+      <nav className="flex px-2 justify-between items-center container mx-auto">
+        <section className="text-2xl">
+          <h2 className="">QuizCode</h2>
+        </section>
+        <section
+          className={`inline-block nav text-2xl bg-orange-200 p-2 w-full  md:bg-blue-100 text-center md:text-right md:static absolute right-[1px] md:right-0  top-16 duration-1000 ease-in-out ${
+            open ? "top-5" : "top-[-520px]"
+          }`}
+        >
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? "md:active bg-red-400 rounded-md p-1" : undefined
+            }
+            to="/home"
+          >
+            Home
+          </NavLink>
+
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? "md:active bg-red-400 rounded-md p-1" : undefined
+            }
+            to="/statistics"
+          >
+            Statistics
+          </NavLink>
+
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? "md:active bg-red-400 rounded-md p-1" : undefined
+            }
+            to="/blog"
+          >
+            Blog
+          </NavLink>
+        </section>
+
+        {/* hamburger menu icon */}
+        <div
+          onClick={() => setOpen(!open)}
+          className="h-8 w-6 cursor-pointer md:hidden "
+        >
+          {open ? <XMarkIcon /> : <Bars3Icon />}
+        </div>
+      </nav>
     </div>
   );
 };
