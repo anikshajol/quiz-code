e7cfacf396be167510870a76a664ba7e97a10307
anikshajol <anikshajol@gmail.com>
2022-10-12 00:27:02 +0600

    eyeIcon added with click handlebar

--- src/Components/Option/Option.js
+++ src/Components/Option/Option.js
@@ -1,12 +1,28 @@
 import React from "react";
+import { EyeIcon } from "@heroicons/react/24/solid";
+import swal from "sweetalert";
 
 const Option = ({ ques }) => {
   const { question, id, core, correctAnswer, options } = ques;
 
+  const handleClickCorrectAnswer = () => {
+    console.log("click");
+    swal(`Correct Answer:${correctAnswer}`);
+  };
   return (
     <div className="">
-      <div className="card w-[500px] glass">
-        <h2 className="text-center text-xl">{question}</h2>
+      <div className="card w-[750px] glass p-5">
+        <section className="flex justify-between">
+          <div className="">
+            <h2 className="text-center text-xl">{question}</h2>
+          </div>
+          <div className="pl-12">
+            <EyeIcon
+              onClick={handleClickCorrectAnswer}
+              className="w-10 cursor-pointer"
+            ></EyeIcon>
+          </div>
+        </section>
         <div className="card-body">
           <section>
             <input className="mr-2 w-10" type="radio" name="option" id="" />
